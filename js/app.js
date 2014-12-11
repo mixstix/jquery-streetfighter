d8acabdab30f0ffb9062e87998371bc675fc853d
mixstix <mixstixx@gmail.com>
2014-12-11 18:36:14 +0800

    Added animation, sound and other effects

--- js/app.js
+++ js/app.js
@@ -8,16 +8,24 @@ $(document).ready(function() {
         $('.ryu-still').show();
     })
     .mousedown(function() {
-    console.log('mousedown');
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
+    $('.hadouken').finish().show()
+    .animate(
+        {'left': '300px'}, 500, function() {
+        $(this).hide();
+        $(this).css('left', '-137px');
+        });
     })
     .mouseup(function() {
-    console.log('mouseup');
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
     });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
